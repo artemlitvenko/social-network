6d563460e3db6755eb6f9210d804b6a03f83cd4c
artemlitvenko <artem.sapiens@gmail.com>
2021-01-18 20:51:49 +0200

    improve posts components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
+import MyPosts from "./components/profile/myposts/MyPosts";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <div className="wrapper">
             <Navbar/>
             <Profile/>
+            <MyPosts />
         </div>
     </div>
   );
--- src/components/profile/myposts/MyPosts.jsx
+++ src/components/profile/myposts/MyPosts.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import './MyPosts.css';
+import Post from "./post/Post";
 
 const MyPosts = () => {
   return (
-      <div className="main">
-        <div className="main-header"></div>
-        <div className="profile-block">
-          <div className="avatar">
-            <img src='https://64.media.tumblr.com/f2101850c6c13c657e0f3fb198ad2a7e/tumblr_oednj7uxsV1rub9fao4_1280.jpg' />
-          </div>
-          <div className="description">
-            <h2>Artem L.</h2>
-            <div className="my-data">
-              <div>Date of Birthday: 9.08</div>
-              <div>City: Dnipro</div>
-            </div>
-          </div>
-        </div>
+      <div>
+        <Post />
       </div>
   )
 }
