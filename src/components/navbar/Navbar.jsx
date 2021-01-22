c978d80c7106562b3fc21406c7844325db9e3754
artemlitvenko <artem.sapiens@gmail.com>
2021-01-22 08:40:53 +0200

    add NavLink

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Navbar.css';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="sidebar">
             <ul>
-                <a href="/profile"><li>Profile</li></a>
-                <a href="/dialogs"><li>Messages</li></a>
+                <NavLink to="/profile"><li>Profile</li></NavLink>
+                <NavLink to="/dialogs"><li>Messages</li></NavLink>
                 <a href=""><li>News</li></a>
                 <a href=""><li>Music</li></a>
                 <a href=""><li>Settings</li></a>
