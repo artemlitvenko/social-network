35f530967edbc86dc572d5de574b293cda6adb1b
artemlitvenko <artem.sapiens@gmail.com>
2021-01-31 12:26:47 +0200

    delete render.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import state, {subscribe} from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from "./render";
-import state from "./redux/state";
+import {addPost, updateNewPostText} from './redux/state';
+
+export let rerenderEntireTree = (state) => {
+    ReactDOM.render( <App appState={state} addPost={addPost} updateNewPostText={updateNewPostText} />, document.getElementById('root'));
+}
+
 
 rerenderEntireTree(state);
+subscribe(rerenderEntireTree);
 reportWebVitals();
